5a2cbe2b66cfa2bae1dd2f32ce52df1d454d39a9
Sergey Antonov <superflasher22@gmail.com>
2022-08-29 23:03:39 +0300

    Minify html

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,17 @@ function template() {
 	return `
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <title>Survivors</title>
-  </head>
-  <body style="margin: 0; overflow: hidden;">
-    <canvas id="c" style="width: 100%; height: 100%;"></canvas>
-    <script src="s.js"></script>
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		<title>Survivors</title>
+	</head>
+	<body style="margin: 0; overflow: hidden;">
+		<canvas id="c" style="width: 100%; height: 100%;"></canvas>
+		<script src="s.js"></script>
+	</body>
 </html>
-`;
+`.replace(/[\n\t]/g, '');
 }
 
 export default {
