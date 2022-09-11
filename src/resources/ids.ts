dee000fe5710bf858db60e39fe0dc0b30bb08ec2
Sergey Antonov <superflasher22@gmail.com>
2022-09-12 01:13:12 +0300

    Add animations

--- src/resources/ids.ts
+++ src/resources/ids.ts
@@ -3,5 +3,11 @@ export const enum Resources {
 	sound_gun = 1,
 	sound_hit = 2,
 	sound_shotgun = 3,
-	walk_forward = 4,
+	attack_zombie_1 = 4,
+	attack_zombie_2 = 5,
+	dead_hero = 6,
+	dead_zombie = 7,
+	walk_forward = 8,
+	walk_right = 9,
+	walk_zombie = 10,
 }
